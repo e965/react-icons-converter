23c8df14d4c0180185be26a8d2c8dfe1ec146b49
Dmitry Dyachkov <git@e965.ru>
2021-06-19 04:23:57 +0500

    1.0.0

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # react-icons-converter
+
+SVG icon converter to format required for use with [react-icons](https://www.npmjs.com/package/react-icons) package.
+
+Inspired by [matthova](https://github.com/matthova)'s [react-icons-json-generator](https://github.com/matthova/react-icons-converter) ([github](https://github.com/matthova/react-icons-converter)). The source code was taken from this project and modified.
+
+¯\\_(ツ)_/¯
+
+## Installation
+
+-   `npm i react-icons-converter`
+
+## Usage example
+
+```js
+import { GenIcon } from 'react-icons';
+import { createIconTreeFromSVG } from 'react-icons-converter';
+
+const SVGSource = ` /* here will be the source code of your svg icon */ `;
+
+const IconTree = createIconTreeFromSVG(SVGSource);
+
+export const Icon = GenIcon(IconTree);
+```
---
+++ src/index.ts
@@ -0,0 +1,79 @@
+import { parse as parseXML } from 'fast-xml-parser';
+import type { X2jOptions as XMLParserOptionsType } from 'fast-xml-parser';
+
+import camelCase from 'camelcase';
+
+// from react-icons/IconBase.d.ts
+export type IconTreeType = {
+    tag: string;
+    attr: {
+        [key: string]: string;
+    };
+    child: IconTreeType[];
+};
+
+export const createIconTreeFromSVG = (svgSource: string): IconTreeType => {
+    const XMLParserOptions: Partial<XMLParserOptionsType> = {
+        ignoreAttributes: false,
+        attributeNamePrefix: '',
+        attrNodeName: 'attributes',
+        textNodeName: '#text',
+        allowBooleanAttributes: true,
+        trimValues: false,
+    };
+
+    const XMLParserOptionForAttr = String(XMLParserOptions.attrNodeName ?? '');
+    const XMLParserOptionForText = String(XMLParserOptions.textNodeName ?? '');
+
+    const SVGObject = parseXML(svgSource, XMLParserOptions);
+
+    const SVGObjectRootNodeName = Object.keys(SVGObject)[0];
+
+    const createTreeAttrObject = (attributes: IconTreeType['attr'], nodeName: string): IconTreeType['attr'] => {
+        return Object.keys(attributes)
+            .filter(attributeName => {
+                return ![
+                    'class',
+                    // if nodeName is svg, then some attributes are removed
+                    ...(nodeName === 'svg' ? ['xmlns', 'xmlns:xlink', 'xml:space', 'width', 'height'] : []),
+                ].includes(attributeName);
+            })
+            .reduce((attrObject: IconTreeType['attr'], attributeName) => {
+                const AttributeValue = attributes[attributeName];
+                const NewAttributeName = camelCase(attributeName);
+
+                switch (NewAttributeName) {
+                    case 'fill': {
+                        if (AttributeValue === 'none') {
+                            attrObject[NewAttributeName] = AttributeValue;
+                        }
+                        break;
+                    }
+                    default: {
+                        attrObject[NewAttributeName] = AttributeValue;
+                        break;
+                    }
+                }
+
+                return attrObject;
+            }, {});
+    };
+
+    const createTreeFromSVGObject = (svgObject: typeof SVGObject, nodeName: string): IconTreeType => {
+        let childrenKeys = Object.keys(svgObject).filter(key => {
+            return !['style', XMLParserOptionForAttr, XMLParserOptionForText, 'defs', 'use'].includes(key);
+        });
+
+        // put at the end of the array of keys
+        if (svgObject.defs) childrenKeys = [...childrenKeys, 'defs'];
+        if (svgObject.use) childrenKeys = [...childrenKeys, 'use'];
+
+        return {
+            tag: nodeName,
+            attr: createTreeAttrObject(svgObject[XMLParserOptionForAttr] ?? {}, nodeName),
+            child: childrenKeys.map(key => createTreeFromSVGObject(svgObject[key], key)),
+        };
+    };
+
+    return createTreeFromSVGObject(SVGObject[SVGObjectRootNodeName], SVGObjectRootNodeName);
+};
